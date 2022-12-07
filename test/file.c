c8f836f4487620bfffe529b6c6d685b097a6e51b
Ksenob1te <Skalini2004>
2022-12-07 13:17:55 +0300

    Mira's code

--- test/file.c
+++ test/file.c
@@ -1,66 +1,72 @@
 #include <stdio.h>
-#include "stdlib.h"
-#include "time.h"
-#include "limits.h"
-#define si short int
+#include <stdlib.h>
+#include <string.h>
+#include <readline/readline.h>
+#define DELIM " \t"
 
-void array_input(int* array, const int* len) {
-    for (int i = 0; i < *len; ++i) {
-        array[i] = rand() % 20000 - 10000;
-//        int status = scanf("%d", array + i);
-//        while (status <= 0) {
-//            if (status == EOF) exit(-1);
-//            printf("Incorrect input, try again: \n");
-//            scanf("%*[^\n]*c");
-//            status = scanf("%d", array + i);
-//        }
-    }
-}
-
-void array_print(const int* array, const int* len) {
-    printf("\nUr array is: \n");
-    for (int i = 0; i < *len; ++i) {
-        printf("%d ", array[i]);
-    }
-}
-
-void array_remove_dupl(int* array, int* answer, si* checker_p, si* checker_n, int* len) {
-    int ans_counter = 0;
-    for (int i = 0; i < *len; ++i) {
-        if (array[i] < 0 && !checker_n[abs(array[i]) - 1]) {
-            checker_n[abs(array[i]) - 1] = 1;
-            answer[ans_counter++] = array[i];
-        }
-        else if (array[i] > 0 && !checker_p[array[i] - 1]) {
-            checker_p[array[i] - 1] = 1;
-            answer[ans_counter++] = array[i];
+short check(char *str1, char *str2) {
+    int count=0;
+    for (int i=0;i<strlen(str1);i++) {
+        for (int j=0;j<strlen(str2);j++) {
+            if (str1[i]==str2[j]) {
+                count+=1;
+                break;
+            }
         }
     }
-    *len = ans_counter;
+    if (count==strlen(str1)) return 1;
+    return 0;
 }
 
 
-int main() {
-    int len = 10000;
-    srand(time(NULL));
-    si* checker_positive = (si*)calloc(0, (10000) * sizeof(si));
-    si* checker_negative = (si*)calloc(0, (10000) * sizeof(si));
-    int* array = (int*)malloc(len * sizeof(int));
-    int* answer = (int*)malloc(len * sizeof(int));
-    if (checker_positive == NULL || checker_negative == NULL || array == NULL || answer == NULL) {
-        printf("kjhfdfs");
-        return 0;
-    }
-//    for (int i = 0; i < 100000; ++i) {
-//        printf("%d ", checker_positive[i]);
-//    }
+char* registr(char *str1) {
+    char* buff=calloc(strlen(str1)+1, sizeof(char));
+    for (int i=0; i<=strlen(str1);i++){
 
-    array_input(array, &len);
-    clock_t start = 0, end = 0;
-    start = clock();
-    array_remove_dupl(array, answer, checker_positive, checker_negative, &len);
-    end = clock();
-    printf("\n%d\n", len);
-    printf("\n%lf", (double)(end-start)/CLOCKS_PER_SEC);
+
+        if((int) str1[i] >= 65 && (int) str1[i] <= 90){
+            buff[i] = (char) ((int) str1[i] + 32);
+        }
+        else buff[i]=str1[i];
+    }
+    return buff;
+}
+
+void editor(char  *str){
+    int res_len=0;
+    char **res=calloc(res_len+1, sizeof(char*));
+    char *word=strtok(str, DELIM);
+    while (word!=NULL){
+        res[res_len] = word;
+        res_len+=1;
+        res= realloc(res,(res_len+1) * sizeof(char*));
+        word=strtok(NULL, DELIM);
+    }
+    for (int i=0;i<res_len;i++) {
+        for (int j=i+1;j<res_len;j++) {
+            if (check(registr(res[i]), registr(res[j]))==1) {
+                char *buff=res[j];
+                for (int g=j-1 ; g>i; g--){
+                    res[g+1]=res[g];
+                }
+                res[i+1]=buff;
+            }
+        }
+    }
+    for (int i=0; i<res_len;i++){
+        printf("%s", res[i]);
+    }
+}
+
+int main(){
+    char *s=readline("> ");
+    char *pshol=strtok(s," \t");
+    printf("%s\n%s\n", s,pshol);
+    while (s != NULL){
+        printf("\"%s\"\n", s);
+        editor(s);
+        free(s);
+        s=readline("> ");
+    }
     return 0;
 }
