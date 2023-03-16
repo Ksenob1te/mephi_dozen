5680f264df709758a347e6ee4701dcf3e7ec8940
IvarysYT <skalin_i_i@mail.ru>
2023-03-16 22:09:50 +0300

    lab 3

--- sem2/lab3/menu/menu.c
+++ sem2/lab3/menu/menu.c
@@ -1,111 +1,65 @@
 #include <stdio.h>
+#include <stdlib.h>
+#ifdef _WIN32
+#include <conio.h>
+#else
 #include <termios.h>
-
-// Функция для установки режима небуферизованного ввода
-void set_raw_input_mode()
-{
-    struct termios tattr;
-    char c;
-
-    // Установить терминал в канонический режим
-    tcgetattr(0, &tattr);
-    tattr.c_lflag &= ~(ICANON | ECHO); // Отключить канонический режим и эхо
-    tattr.c_cc[VMIN] = 1; // Минимальное количество символов для чтения
-    tattr.c_cc[VTIME] = 0; // Максимальное время ожидания между символами
-    tcsetattr(0, TCSAFLUSH, &tattr);
+#include <unistd.h>
+int getch(void) {
+    struct termios oldattr, newattr;
+    int ch;
+    tcgetattr(STDIN_FILENO, &oldattr);
+    newattr = oldattr;
+    newattr.c_lflag &= ~(ICANON | ECHO);
+    tcsetattr(STDIN_FILENO, TCSANOW, &newattr);
+    ch = getchar();
+    tcsetattr(STDIN_FILENO, TCSANOW, &oldattr);
+    return ch;
 }
+#endif
 
-// Функция для отображения меню и выделения текущего пункта
-void display_menu(int current)
-{
-    char *menu[] = {"Пункт 1", "Пункт 2", "Пункт 3", "Выход"};
-    int size = sizeof(menu) / sizeof(menu[0]);
+#define MAX_OPTIONS 3
 
-    printf("\033[H\033[J"); // Очистить экран
-    printf("Выберите пункт меню:\n");
+int main() {
+    int selected_option = 0;
+    int key_code;
 
-    for (int i = 0; i < size; i++)
-    {
-        if (i == current) // Если текущий пункт - выделить его цветом
-        {
-            printf("\033[30;47m%s\033[0m\n", menu[i]); // Черный текст на белом фоне
+    while (1) {
+        system("cls||clear");
+        printf("Use arrow keys to navigate the menu and press Enter to select an option.\n\n");
+
+        for (int i = 0; i < MAX_OPTIONS; i++) {
+            if (i == selected_option) printf("> ");
+            else printf("  ");
+            printf("Option %d\n", i + 1);
         }
-        else // Иначе - обычный текст
-        {
-            printf("%s\n", menu[i]);
+
+        key_code = getch();
+
+#ifdef _WIN32
+        if (key_code == 224) key_code = getch();
+#endif
+
+        switch (key_code) {
+            case 'w':
+            case 'W':
+            case 72:
+                selected_option--;
+                break;
+            case 's':
+            case 'S':
+            case 80:
+                selected_option++;
+                break;
+            case '\r':
+                printf("\nOption %d selected!\n", selected_option + 1);
+                getch();
+                break;
         }
+
+        if (selected_option >= MAX_OPTIONS) selected_option = MAX_OPTIONS - 1;
+        else if (selected_option < 0) selected_option = 0;
 
     }
-}
-
-// Функция для обработки выбора пользователя
-void handle_choice(int choice)
-{
-    switch (choice)
-    {
-        case 0:
-            printf("Вы выбрали пункт 1\n");
-            break;
-        case 1:
-            printf("Вы выбрали пункт 2\n");
-            break;
-        case 2:
-            printf("Вы выбрали пункт 3\n");
-            break;
-        case 3:
-            printf("Вы выбрали выход\n");
-            break;
-        default:
-            printf("Неверный выбор\n");
-            break;
-    }
-}
-
-int main()
-{
-    int current = 0; // Текущий выбранный пункт меню
-    int done = 0; // Флаг завершения программы
-    char c; // Символ для чтения ввода
-
-    set_raw_input_mode(); // Установить режим небуферизованного ввода
-
-    display_menu(current); // Отобразить меню
-
-    while (!done) // Пока не завершено
-    {
-        c = getchar(); // Прочитать символ
-
-        if (c == '\033') // Если символ - escape
-        {
-            getchar(); // Пропустить следующий символ '['
-            c = getchar(); // Прочитать код клавиши со стрелкой
-
-            switch (c)
-            {
-                case 'A': // Если вверх
-                    current--; // Уменьшить текущий выбор на 1
-                    if (current < 0) current = 0; // Не выходить за границы меню
-                    break;
-                case 'B': // Если вниз
-                    current++; // Увеличить текущий выбор на 1
-                    if (current > 3) current = 3; // Не выходить за границы меню
-                    break;
-                default:
-                    break;
-            }
-
-            display_menu(current); // Отобразить меню с новым выбором
-
-        }
-        else if (c == '\n') // Если символ - enter
-        {
-            handle_choice(current); // Обработать выбор пользователя
-
-            if (current == 3) done = 1; // Если выбран выход - завершить программу
-
-        }
-
-    }
-
-    return 0;
+    return EXIT_SUCCESS;
 }
