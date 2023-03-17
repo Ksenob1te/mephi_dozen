2bc6c736c96165d921b3ba09c4e90e276a8dc562
Ksenob1te <Skalini2004>
2023-03-17 11:46:14 +0300

    lab3 menu

--- sem2/lab3/menu/menu.c
+++ sem2/lab3/menu/menu.c
@@ -108,7 +108,7 @@ int main() {
             handle_choice(current);
             if (current == 3) {done = 1;}
             set_input_mode();
-            scanf("%*[\n]");
+            scanf("%*[^\n]%*[\n]");
         }
     }
     reset_input_mode();
