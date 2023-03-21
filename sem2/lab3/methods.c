5a60260dc9ed4f6f0c642bcf2f1842de4e1e9bb4
Ksenob1te <Skalini2004>
2023-03-22 00:55:40 +0300

    lab3 methods

--- sem2/lab3/methods.c
+++ sem2/lab3/methods.c
@@ -6,12 +6,12 @@ void print_table(Table *table) {
     printf(".\n");
     KeySpace *current = table->head;
     for (; current; current = current->link) {
-        if (current->link) printf("?"); else printf("?");
-        printf("?? %llu\n", current->key);
+        if (current->link) printf("├"); else printf("└");
+        printf("── %llu\n", current->key);
         Node *node = current->node;
         for (; node; node = node->next) {
-            if (node->next) printf("?   ??? %llu\n", node->info);
-            else printf("?   ??? %llu\n", node->info);
+            if (node->next) printf("│   └── %llu\n", node->info);
+            else printf("│   ├── %llu\n", node->info);
         }
     }
 }
