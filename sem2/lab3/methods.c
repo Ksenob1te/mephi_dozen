1731dbeaa57b473c0e7c79e94010492b6bd9af6d
Ksenob1te <Skalini2004>
2023-03-22 09:42:20 +0300

    lab3 methods

--- sem2/lab3/methods.c
+++ sem2/lab3/methods.c
@@ -3,7 +3,7 @@
 #include "stdio.h"
 
 void print_table(Table *table) {
-    printf("\033[0;31m.\033[0m\n");
+    printf("\033[0;33m.\033[0m\n");
     KeySpace *current = table->head;
     for (; current; current = current->link) {
         if (current->link) printf("├"); else printf("└");
