a9aa41591e0b281b6ca68ae5e978c0076c0bba1d
Ksenob1te <Skalini2004>
2023-03-28 13:46:30 +0300

    fix: lab3 last release on removal

--- sem2/lab3/table/table.c
+++ sem2/lab3/table/table.c
@@ -56,7 +56,6 @@ int remove_node_keyspace (struct KeySpace *key, Node *node) {
 
     if (!find_node) return 0;
     find_node->next = node->next;
-    (key->last_release)--;
     removeNode(node);
     return 1;
 }
