ca2bd1ee2e2254e6bf19d39e5f86b3ca4f4f8aca
Ksenob1te <Skalini2004>
2023-05-03 09:36:27 +0300

    refractor new branch

--- sem2/lab2/task/input.h
+++
@@ -1,8 +0,0 @@
-#ifndef SEM1_LAB4_INPUT_H
-#define SEM1_LAB4_INPUT_H
-#include "../structs/task.h"
-
-int isInt(const char *s);
-long strToInt(const char *s);
-Queue * input_array(size_t size);
-#endif //SEM1_LAB4_INPUT_H
